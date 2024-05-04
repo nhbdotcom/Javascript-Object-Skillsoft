67bb39f77052b460c2439a35640a0cd43dfcf177
Norman <nhb_jr@yahoo.com>
2024-05-04 23:03:13 +0800

    Initial commit

---
+++ Javascript Objects/main.js
@@ -0,0 +1,22 @@
+"use strict";
+/*
+/*let firstItem = {
+/*    id:1,
+/*    name: "laptop",
+/*    price: 500
+/*}
+/*
+/*let secondItem = {
+/*    id: 2,
+/*    name: "watch",
+/*    price: 240,
+/*    model: "Seiko"
+/*}
+/*
+/*console.log(`The name of the firstItem is ${firstItem.name} and the price is ${firstItem.price}.`)
+/*
+/*console.log(`The name of the secondItem is ${secondItem["name"]} and the price is ${secondItem["price"]}.`)// you can also do it like this
+/ console.log(`The name of the secondItem is ${secondItem.name} and the price is ${secondItem.price}.`)
+/*
+/*console.log(`The name of the firstItem is ${firstItem["name"]}, while the secondItem is ${secondItem.name}.`);
+*/
